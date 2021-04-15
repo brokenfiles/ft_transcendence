96124caa407a225483de44f1cb9b61a0a1fb3760
brokenfiles <louis.nevysup@gmail.com>
2021-04-15 10:53:10 +0200

    Maybe working fine

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
+
+# fix rails bug
 rm -f /myapp/tmp/pids/server.pid
+
+# install dependencies
+bundle install
+yarn install
+
+# create and migrate db
+rails db:create
+rails db:migrate
+rails db:seed
+
+# exec container main process
 exec "$@"
