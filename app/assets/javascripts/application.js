fef18e63ceb5f269c3632182a81df42c041d6ac6
brokenfiles <louis.nevysup@gmail.com>
2021-05-03 16:38:58 +0200

    Particles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,18 @@
-//= require_tree .
+//= require particles.min
+//= require particles.json
 //= require_self
+//= require_tree .
+
+const $navbar = document.getElementById('navbar')
+const $sidebar = document.getElementById('sidebar')
+const navHeight = $navbar.offsetHeight
+const sideWidth = $sidebar.offsetWidth
+
+const $app = document.getElementById('app')
+$app.style['padding-top'] = `${navHeight}px`
+$app.style['padding-left'] = `${sideWidth}px`
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
 
--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,5 +1,1 @@
-const $navbar = document.getElementById('navbar')
-const $sidebar = document.getElementById('sidebar')
-const navHeight = $navbar.offsetHeight
-
 $sidebar.style.top = `${navHeight}px`
